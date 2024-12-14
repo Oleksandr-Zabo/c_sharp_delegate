2052bb2cb027db2d42299251349a4360c31872f5
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-12-14 14:27:01 +0200

    add program.cs from 2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,53 @@
 ﻿namespace c_sharp_delegate
 {//lab ex. 4
-    internal class Program
+    public delegate double CalcDelegate(double x, double y);
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator calc = new Calculator();
+            Console.Write("Enter an expression: ");
+            string? expression = Console.ReadLine();
+            if (expression == null)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+            char sign = ' ';
+
+            foreach (char item in expression)
+            {
+                if (item == '+' || item == '-' || item == '*')
+                {
+                    sign = item;
+                    break;
+                }
+            }
+            try
+            {
+                string[] numbers = expression.Split(sign);
+                CalcDelegate? del = null;
+                switch (sign)
+                {
+                    case '+':
+                        del = new CalcDelegate(calc.Add);
+                        break;
+                    case '-':
+                        del = new CalcDelegate(Calculator.Sub);
+                        break;
+                    case '*':
+                        del = calc.Mult;
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+                Console.WriteLine($"Result: {del(double.Parse(numbers[0]), double.Parse(numbers[1]))}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
