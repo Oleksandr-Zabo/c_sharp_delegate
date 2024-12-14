b41c2d676a7f263fb7c84bb7a1ad3cb4d24e2bea
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-12-14 13:46:22 +0200

    lab ex. 2 had done

---
+++ Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace c_sharp_delegate
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Sub(double x, double y)
+        {
+            return x - y;
+        }
+        public double Mult(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,55 @@
-﻿namespace c_sharp_delegate
-{//dev
-    internal class Program
+﻿using System;
+
+namespace c_sharp_delegate
+{//lab ex. 2
+    public delegate double CalcDelegate(double x, double y);
+    
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculator calc = new Calculator();
+            Console.Write("Enter an expression: ");
+            string? expression = Console.ReadLine();
+            if (expression == null)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+            char sign = ' ';
+            
+            foreach (char item in expression)
+            {
+                if (item == '+' || item == '-' || item == '*')
+                {
+                    sign = item;
+                    break;
+                }
+            }
+            try
+            {
+                string[] numbers = expression.Split(sign);
+                CalcDelegate? del = null;
+                switch (sign)
+                {
+                    case '+':
+                        del = new CalcDelegate(calc.Add);
+                        break;
+                    case '-':
+                        del = new CalcDelegate(Calculator.Sub);
+                        break;
+                    case '*':
+                        del = calc.Mult;
+                        break;
+                    default:
+                        throw new InvalidOperationException();
+                }
+                Console.WriteLine($"Result: {del(double.Parse(numbers[0]), double.Parse(numbers[1]))}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
