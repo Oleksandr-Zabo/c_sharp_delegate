53c731c96a96a722f4aae80c883501f3557de21d
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-12-14 13:36:41 +0200

    ex. 1 lab had done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace c_sharp_delegate
-{//dev
+{//lab ex.1 
     internal class Program
     {
+        public delegate void MessageDelegate(string message);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create delegate instance and assign them to methods
+            MessageDelegate message = DisplayMessage;
+            message += PrintMessage;
+            message += LogMessage;
+
+            // Invoke the methods through the delegate
+            message("Hello, World!");
+
+            Console.ReadLine();
+        }
+
+        // Method to display a message on the screen
+        static void DisplayMessage(string message)
+        {
+            Console.WriteLine("DisplayMessage: " + message);
+        }
+
+        // Method to print a message
+        static void PrintMessage(string message)
+        {
+            Console.WriteLine("PrintMessage: " + message);
+        }
+
+        // Method to log a message
+        static void LogMessage(string message)
+        {
+            Console.WriteLine("LogMessage: " + message);
         }
     }
 }
+
